a63a00a78fce02c779319c8ac0fc59b3a1154379
Jaromír Beneš <jaromir.benes@gmail.com>
2022-07-20 10:47:02 +0200

    Update mytest.m

--- mytest.m
+++ mytest.m
@@ -4,6 +4,6 @@ ver
 disp ***
 
 x = {1,2,"a",struct('xxx',nan)};
-writematrix(jsonencode(), 'test.json', filetype='text', quoteStrings=true)
+writematrix(jsonencode(x), 'test.json', filetype='text', quoteStrings=true)
 
 type test.json
